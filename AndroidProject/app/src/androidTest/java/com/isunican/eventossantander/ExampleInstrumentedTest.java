a12666dbbcdbae8b93e3afd1cb6cd38a2efe497c
juanmrivas <rivasjm@unican.es>
2021-07-19 13:07:21 +0200

    github actions config

--- AndroidProject/app/src/androidTest/java/com/isunican/eventossantander/ExampleInstrumentedTest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/eventossantander/ExampleInstrumentedTest.java
@@ -2,12 +2,22 @@ package com.isunican.eventossantander;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.core.app.ActivityScenario;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.isunican.eventossantander.view.events.EventsActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.anything;
 import static org.junit.Assert.*;
 
 /**
@@ -17,10 +27,12 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<EventsActivity> activityRule = new ActivityScenarioRule(EventsActivity.class);
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.isunican.eventossantander", appContext.getPackageName());
+    public void openEventFromEventsActivity() {
+        onView(withId(R.id.eventsListView)).perform(click());
     }
 }
